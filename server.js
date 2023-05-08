f20dd26d71ac639e4b4edbf0e4fcc68c88fd5ac8
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-05-08 15:48:59 -0600

    changed server routes

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.static('./public'))
 app.use('/api', apiRoutes)
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, 'notes.html'))
 );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 );
 
 app.listen(PORT, () => console.log('listening'), PORT)
