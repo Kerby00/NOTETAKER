e05592b17ec894b8a56a5d062d5ead8c0b5b8b00
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-05-08 15:57:36 -0600

    undid changes that were made to in attmept to make the github deployed page look nice

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.static('./public'))
 app.use('/api', apiRoutes)
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.listen(PORT, () => console.log('listening'), PORT)
